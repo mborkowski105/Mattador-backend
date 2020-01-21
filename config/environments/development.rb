ebe1265ebfba6825382801dd9035a22fbb6a73b6
Matthew <barnabytheplumber@gmail.com>
2020-01-21 18:13:12 -0500

    Good to present. No categories or auth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.hosts << "fca6a0fa.ngrok.io"
+  config.hosts << "0ffebb21.ngrok.io"
 
 
   # Raises error for missing translations.
